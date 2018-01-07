80bdfde0c77414e67d66f554ec6d14d39cb3e72b
Nick Barth <nbarth@goauto.ca>
2018-01-06 20:57:28 -0700

    fixed class

--- chooser.go
+++ chooser.go
@@ -1,128 +1,120 @@
 package main
 
 import (
-	_ "fmt"
-	"github.com/renstrom/fuzzysearch/fuzzy"
 	"golang.org/x/crypto/ssh/terminal"
+	"io"
+	"math/rand"
 	"os"
-	"path/filepath"
-	"sort"
 )
 
-func enter(term *terminal.Terminal) {
-	// term.Write([]byte{'\n'})         // ENTER
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'G'}) // START OF LINE
+type Chooser struct {
+	height  int
+	width   int
+	options []string
+	term    *terminal.Terminal
+	r       io.Reader
 }
 
-func clear(term *terminal.Terminal) {
-	term.Write([]byte{'\n'})
-	term.Write([]byte{'\n'})
-	term.Write([]byte{'\n'})
-	term.Write([]byte{'\n'})
-	term.Write([]byte{'\n'})
-}
+func NewChooser() *Chooser {
+	width, _, _ := terminal.GetSize(0)
+	term := terminal.NewTerminal(os.Stdin, "")
 
-func out(term *terminal.Terminal) {
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'B'}) // DOWN
-	term.Write([]byte{27, '[', 'G'}) // START OF LINE
-}
-
-func top(term *terminal.Terminal) {
-	term.Write([]byte{27, '[', 'A'})      // UP
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte{27, '[', 'A'})      // UP
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte{27, '[', 'A'})      // UP
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte{27, '[', 'A'})      // UP
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte{27, '[', 'A'})      // UP
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte{27, '[', 'G'})      // START OF LINE
-}
-
-func bottom(term *terminal.Terminal) {
-	term.Write([]byte{27, '[', 'B'}) // UP
-	term.Write([]byte{27, '[', 'B'}) // UP
-	term.Write([]byte{27, '[', 'B'}) // UP
-	term.Write([]byte{27, '[', 'B'}) // UP
-	term.Write([]byte{27, '[', 'B'}) // UP
-	term.Write([]byte{27, '[', 'G'}) // START OF LINE
-}
-
-func printOptions(term *terminal.Terminal, find string, files []string) {
-	matches := fuzzy.RankFind(find, files)
-	sort.Sort(matches)
-	sort.Sort(sort.Reverse(matches))
-
-	for i := 0; i < 5-len(matches); i++ {
-		enter(term)
+	return &Chooser{
+		r:       os.Stdin,
+		term:    term,
+		height:  5,
+		width:   width,
+		options: []string{"one", "two", "three", "four", "five", "six", "seven"},
 	}
+}
 
-	for n, match := range matches {
-		if n > 5 {
+var (
+	tcEscape    = byte(27)
+	tcUp        = []byte{tcEscape, '[', 'A'}
+	tcDown      = []byte{tcEscape, '[', 'B'}
+	tcClearLine = []byte{tcEscape, '[', '2', 'K'}
+	tcLineStart = []byte{tcEscape, '[', 'G'}
+	tcReturn    = []byte{'\n'}
+)
+
+func (c Chooser) Choose() {
+	for n := 0; n < c.height-1; n++ {
+		c.term.Write(tcReturn)
+	}
+	c.clear()
+	c.draw()
+	c.readInput()
+}
+
+func (c Chooser) writeln(ln string) {
+	c.writeRaw(tcLineStart)
+	c.write(ln)
+	c.writeRaw(tcDown)
+}
+
+func (c Chooser) write(str string) {
+	c.term.Write([]byte(str))
+}
+
+func (c Chooser) writeRaw(str []byte) {
+	c.term.Write([]byte(str))
+}
+
+func (c Chooser) clear() {
+	for n := 0; n < c.height-1; n++ {
+		c.writeRaw(tcUp)
+		c.writeRaw(tcClearLine)
+	}
+}
+
+func (c Chooser) drawPrompt() {
+	c.writeRaw(tcLineStart)
+	c.write("> ")
+}
+
+func (c Chooser) draw() {
+	n := 0
+	for n = 0; n < len(c.options); n++ {
+		if n > (c.height - 2) {
 			break
 		}
-		term.Write([]byte(match.Target))
-		enter(term)
+		c.writeln(c.options[n])
 	}
-
-	term.Write([]byte{27, '[', '2', 'K'}) // CLEAR LINE
-	term.Write([]byte("> "))              // ENTER
+	c.drawPrompt()
 }
 
-func main() {
-	// grab files
-	files, err := filepath.Glob("*")
-	if err != nil {
-		panic(err)
+func (c *Chooser) sortOptions() {
+	for n := range c.options {
+		m := rand.Intn(n + 1)
+		c.options[n], c.options[m] = c.options[m], c.options[n]
 	}
+}
 
-	// create terminal
-	oldstate, _ := terminal.MakeRaw(0)
-	// width, height, _ := terminal.GetSize(0)
-	defer terminal.Restore(0, oldstate)
-
-	term := terminal.NewTerminal(os.Stdin, "")
-	// enter(term)
-	clear(term)
-	top(term)
-	printOptions(term, "", files)
-
-	var resultBuf []byte
+func (c Chooser) readInput() {
+	var result []byte
 	var f = os.Stdin
+
 	for {
 		var buf [1]byte
-
 		n, _ := f.Read(buf[:])
-		// if err != nil && err != io.EOF { return "", err }
 
 		if n == 0 || buf[0] == '\n' || buf[0] == '\r' {
 			break
 		}
 
-		if buf[0] == byte(127) {
-			if len(resultBuf) > 1 {
-				resultBuf = resultBuf[:len(resultBuf)-1]
-			} else if len(resultBuf) == 1 {
-				resultBuf = []byte{}
-			}
-		} else if buf[0] == byte(21) || buf[0] == byte(11) || buf[0] == byte(23) || buf[0] == byte(12) {
-			resultBuf = []byte{}
-		} else {
-			resultBuf = append(resultBuf, buf[0])
-		}
-
-		top(term)
-		printOptions(term, string(resultBuf), files)
-		term.Write([]byte(resultBuf))
+		result = append(result, buf[0])
+		c.clear()
+		c.sortOptions()
+		c.draw()
 	}
 
-	term.Write([]byte("\n"))
-	enter(term)
+	c.write("\n")
+}
+
+func main() {
+	oldstate, _ := terminal.MakeRaw(0)
+	defer terminal.Restore(0, oldstate)
+
+	chooser := NewChooser()
+	chooser.Choose()
 }
